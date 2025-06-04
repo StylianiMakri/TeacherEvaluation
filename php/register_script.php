9a4a48f037da516569bfd158b83823335e7e6db1
Στέλλα Μακρή <132708484+StylianiMakri@users.noreply.github.com>
2025-06-04 16:52:20 +0300

    Updates

--- php/register_script.php
+++ php/register_script.php
@@ -1,13 +1,10 @@
 <?php
-// Database connection
 $conn = mysqli_connect("localhost","root" ,"" , "teacherevaluationsystem");
 
-// Check connection
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
 
-// Get form data
 $type = $_POST['type'];
 $email = $_POST['email'];
 $password = $_POST['password'];
@@ -17,10 +14,8 @@ $dateofbirth = $_POST['dateofbirth'];
 $yearofstudy = $_POST['yearofstudy'];
 $phone = $_POST['phone'];
 
-// Hash the password
 $hashed_password = password_hash($password, PASSWORD_DEFAULT);
 
-// Insert the data into the database
 if ($type == 'student') {
     $sql = "INSERT INTO students (email, password, name, lastname, dateofbirth, yearofstudy, phone) VALUES ('$email', '$hashed_password', '$name', '$lastname', '$dateofbirth', '$yearofstudy', '$phone')";
 } else {
@@ -33,6 +28,5 @@ if (mysqli_query($conn, $sql)) {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-// Close the connection
 mysqli_close($conn);
 ?>
