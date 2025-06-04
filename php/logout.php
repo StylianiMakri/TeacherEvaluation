483f2847e821c0a66bcf09c1ac0a220fe1abee9e
Στέλλα Μακρή <132708484+StylianiMakri@users.noreply.github.com>
2025-06-04 16:52:02 +0300

    Updates

--- php/logout.php
+++ php/logout.php
@@ -1,14 +1,10 @@
 <?php
-// Start the session
 session_start();
 
-// Unset all session variables
 $_SESSION = array();
 
-// Destroy the session
 session_destroy();
 
-// Redirect to the login screen
 header("Location: http://localhost/TeacherEvaluation/index.html");
 exit();
 ?>
